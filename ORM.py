439c9d17ed4b7036d47f497a44c0257145546c44
JeanVanneste <jean.vanneste@student.vinci.be>
2018-12-25 17:58:52 +0100

    Connection db

--- ORM.py
+++ ORM.py
@@ -1,1 +1,7 @@
-import sqlalchemy
+from sqlalchemy import create_engine
+
+engine = create_engine(
+    'mysql+pymysql://jean:mcsuaptesbuf@localhost/library', echo = True)
+
+if __name__ == "__main__":
+    print("ok")
