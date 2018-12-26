cd9de2d6a91951e209890a9dc188a346c2b38a61
JeanVanneste <jean.vanneste@student.vinci.be>
2018-12-26 19:09:09 +0100

    Create new classes

--- ORM.py
+++ ORM.py
@@ -1,4 +1,6 @@
 from sqlalchemy import create_engine
+from classes import Book, Editor, Collection, Author
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
     'mysql+pymysql://jean:mcsuaptesbuf@localhost/library', echo = True)
