87b598161085cf9b439ef36ba9ec6b07ea86c8e2
JeanVanneste <jean.vanneste@student.vinci.be>
2018-12-28 23:00:48 +0100

    Adding create_editor method (don't work)

--- ORM.py
+++ ORM.py
@@ -3,5 +3,21 @@ import database
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+engine = create_engine('mysql+pymysql://jean:mcsuaptesbuf@localhost/library', echo = True)
+Session = sessionmaker(engine)
+
+
+def create_editor(editorName, fondationYear):
+    session = Session()
+
+    editor = database.Editor()
+
+    editor.name = editorName
+    editor.fondationYear = fondationYear
+
+    session.add(editor)
+    session.commit()
+
 if __name__ == "__main__":
-    print("ok")
+    print(engine)
+    create_editor('Gallimard', 1911)
