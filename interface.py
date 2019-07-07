787ef3bcabb85d61a4589d43319edb509f46b897
JeanVanneste <jean.vanneste@student.vinci.be>
2019-07-07 20:26:45 +0200

    Functions add_author, create_book

--- interface.py
+++ interface.py
@@ -18,18 +18,38 @@ def create_author():
     print(authorFirstName + " " + authorLastName + " ajouté·e")
     ORM.author_create(authorFirstName, authorLastName)
 
+def create_book():
+    title = input("Quel est le titre du livre?\n")
+    isbn = input("Quel est le code ISBN du livre ?\n")
+    authorFirstName = input("Quel est le prénom du premier auteur ?\n")
+    authorLastName = input("Quel est le nom du premier auteur ?\n")
+    publicationYear = input("En quelle année le livre a-t-il été publié ?\n")
+    collection = input("À quelle collection le livre appartient-il ?\n")
+    print(title + " ajouté")
+    ORM.book_create(title, isbn, publicationYear, collection, authorFirstName, authorLastName)
+
+def add_author():
+    title = input("Titre du livre :\n")
+    firstName = input("Prénom de l'auteur :\n")
+    lastName = input("Nom de l'auteur :\n")
+    ORM.book_add_author(title, firstName, lastName)
+
 if __name__ == "__main__":
-    
+
     while (True):
         print("1. Créer un éditeur")
         print("2. Créer une collection")
         print("3. Créer un auteur")
+        print("4. Créer un livre")
+        print("5. Ajouter un auteur à un livre")
 
         option = input()
 
         if (option == "1") : create_editor()
         elif (option == "2") : create_collection()
         elif (option == "3") : create_author()
+        elif (option == "4") : create_book()
+        elif (option == "5") : add_author()
         else: 
             print("Option invalide")
             break
